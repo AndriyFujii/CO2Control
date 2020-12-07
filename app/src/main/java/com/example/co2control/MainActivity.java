6d0b0670e8718d8a95d42f7949b2495e5a7c24c2
Andriy Fuji Pendloski <afp@edu.univali.br>
2020-12-07 15:56:46 -0300

    Read values from editText and switch widgets and set error if editText is empty

--- app/src/main/java/com/example/co2control/MainActivity.java
+++ app/src/main/java/com/example/co2control/MainActivity.java
@@ -3,21 +3,112 @@ package com.example.co2control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText etCO2Threshold;
+
+    Switch switchExhauster;
+    Switch switchWindow;
+    Switch switchAutomatic;
+    boolean boolExhauster;
+    boolean boolWindow;
+    boolean boolAutomatic;
+
+    Button bSave;
+
+    int sensorID;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        bSave = findViewById(R.id.bSave);
+        etCO2Threshold = findViewById(R.id.etCO2Threshold);
+        switchExhauster = findViewById(R.id.switchExhauster);
+        switchWindow = findViewById(R.id.switchWindow);
+        switchAutomatic = findViewById(R.id.switchAutomatic);
 
-        myRef.setValue("Hello, World!");
+        sensorID = 1;
+
+        switchExhauster.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if(isChecked)
+                    boolExhauster = true;
+                else
+                    boolExhauster = false;
+            }
+        });
+
+        switchWindow.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if(isChecked)
+                    boolWindow = true;
+                else
+                    boolWindow = false;
+            }
+        });
+
+        switchAutomatic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if(isChecked)
+                    boolAutomatic = true;
+                else
+                    boolAutomatic = false;
+            }
+        });
+    }
+
+    //Verifies if the editText is empty and pops an error if it is
+    //Receives the editText as parameter
+    //Returns true if it's empty
+    public boolean isEmpty(EditText et)
+    {
+        String validation = et.getText().toString();
+        if(TextUtils.isEmpty(validation))
+        {
+            et.setError("Can't be empty");
+            return true;
+        }
+
+        return false;
+    }
+
+    public void onClickSave(View v)
+    {
+        String value = etCO2Threshold.getText().toString();
+
+        boolean error = false;
+        if(isEmpty(etCO2Threshold))
+            error = true;
+
+        if(!error)
+        {
+            int CO2Threshold = Integer.parseInt(value);
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference(sensorID + "/test");
+            myRef.setValue("Hello, World!");
+        }
     }
 }
